580abe90409f7f03b772f5aab6ccf5884f4c77fe
Enja Kulas <enja.kulas@gmail.com>
2020-05-13 14:28:18 +0200

    Vzpostavitevzačetnega stanja

---
+++ model.py
@@ -0,0 +1,58 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = '+'
+PONOVLJENA_CRKA = 'o'
+NAPACNA_CRKA = '-'
+ZMAGA = 'w'
+PORAZ = 'x'
+
+class Igra: 
+    geslo = ""
+    crke = []
+    def __init__(self, geslo, crke):
+        self.geslo = geslo
+        self.crke = crke
+    def napacne_crke():
+        return napacna_ugibanja
+
+    def pravilne_crke():
+        return pravilna_ugibanja
+
+    def stevilo_napak():
+        return
+
+    def zmaga():
+        return
+
+    def poraz():
+        return
+
+    def pravilni_del_gesla():
+        return
+
+    def nepravilni_ugibi():
+        return
+
+    def ugibaj(crka):
+        return
+
+def Igra.stevilo_napak():
+
+def ugani_crka_model(igra, crka):
+    igro.odkrite.add(crka)
+    pravilno = crka in igra.geslo
+    if not pravilno
+        obesen += 1
+    return pravilno
+
+class Igra:
+    def ugani_crko(elf, crka):
+        self-odkrite-add(crka)
+        pravilno = crka in self.geslo
+        if not pravilno:
+            obesen +=  1
+        return pravilno
+
+#PIŠI KOMENTARJE, NUJNO!!!
+
+
+
